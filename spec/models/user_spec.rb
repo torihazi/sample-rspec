d8ee3aa993fac7293b97fe32474c5e062d3800f1
torihazi <lovebarusa19981225@icloud.com>
2025-11-23 19:27:29 +0900

    feat user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  # pendingってなんでしょうね。
+  # とりあえず describeというのがまとまりなのかな
+  it "is valid with a first name, last name, email, and password"
+
+  it "is invalid without a first name"
+
+  it "is invalid without a last name"
+
+  it "is invalid without an email"
+
+  it "is invalid with a duplicate email"
+
+  it "returns a user's full name as a string"
 end

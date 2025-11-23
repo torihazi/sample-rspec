aa263ec7200fdfe5aeab2bbe895def1eca287aec
torihazi <lovebarusa19981225@icloud.com>
2025-11-23 19:52:08 +0900

    impl user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
 
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end

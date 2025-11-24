2147c6e97e018951ef3a737826f611dbd4b4fb22
torihazi <lovebarusa19981225@icloud.com>
2025-11-24 19:59:10 +0900

    fin

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -23,4 +23,32 @@ RSpec.describe Project, type: :model do
     expect(new_project.errors[:name]).to include("has already been taken")
     
   end
+
+  # 二人のユーザーが同じ名前を使うことは許可する
+  it "allows two users to share a project name" do
+    user1 = User.create(
+      first_name: "John",
+      last_name: "Doe",
+      email: "john.doe@example.com",
+      password: "password"
+    )
+
+    user1.projects.create(
+      name: "Project 1",
+      description: "This is the first project"
+    )
+
+    user2 = User.create(
+      first_name: "Jane",
+      last_name: "Smith",
+      email: "jane.smith@example.com",
+      password: "password"
+    )
+
+    other_project = user2.projects.build(
+      name: "Project 1",
+      description: "This is another project with the same name"
+    )
+    expect(other_project).to be_valid
+  end
 end

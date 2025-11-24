aa9d5bb2a6c21079cd44a95a12a486331d31b8c7
torihazi <lovebarusa19981225@icloud.com>
2025-11-24 20:20:24 +0900

    add failed patterns

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -32,4 +32,23 @@ RSpec.describe Note, type: :model do
     expect(Note.search("second")).to include(note2)
     expect(Note.search("third")).not_to include(note1, note2, note3)
   end
+
+  # 検索結果が1件も⾒つからなければ空のコレクションを返すこと
+  it "returns an empty collection when no results are found" do
+    user = User.create(
+      first_name: "John",
+      last_name: "Doe",
+      email: "john.doe@example.com",
+      password: "password"
+    )
+    project = user.projects.create(
+      name: "Project 1",
+      description: "This is the first project"
+    )
+    note = project.notes.create(
+      message: "This is the first note",
+      user_id: user.id
+    )
+    expect(Note.search("nonexistent")).to be_empty
+  end
 end
